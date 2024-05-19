ef6a044ba0ace6e56c0219a3273241a827ee53c0
avfernandez03 <167803504+avfernandez03@users.noreply.github.com>
2024-05-19 19:40:19 +0200

    Added current location function to dropdown

--- javascript/index.js
+++ javascript/index.js
@@ -2,25 +2,32 @@
 
 function updateTime() {
     let losAngelesElement = document.querySelector("#los-angeles");
-    let losAngelesDateElement = document.querySelector(".date");
-    let losAngelesTimeElement = document.querySelector(".time");
-    let losAngelesTime = moment().tz("America/Los_Angeles");
-    
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-    losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+    if (losAngelesElement) {
+      let losAngelesDateElement = document.querySelector(".date");
+      let losAngelesTimeElement = document.querySelector(".time");
+      let losAngelesTime = moment().tz("America/Los_Angeles");
+      
+      losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+      losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");  
+    }
 
     let parisElement = document.querySelector("#paris");
-    let parisDateElement = parisElement.querySelector(".date");
-    let parisTimeElement = parisElement.querySelector(".time");
-    let parisTime = moment().tz("Europe/Paris");
-
-    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-    parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]")
-
+    if (parisElement) {
+      let parisDateElement = parisElement.querySelector(".date");
+      let parisTimeElement = parisElement.querySelector(".time");
+      let parisTime = moment().tz("Europe/Paris");
+  
+      parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+      parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+  
+    }
 }
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
